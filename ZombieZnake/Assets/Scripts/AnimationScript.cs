8d97fabfd0064a7b0c7be71c9fb03e58783d7827
Oscar Wilhelmsson <karloscarwilhelmsson@gmail.com>
2021-02-04 10:39:16 +0100

    Added animations

---
+++ ZombieZnake/Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
